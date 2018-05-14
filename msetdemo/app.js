a46397120742b8e98ba606455900c574cb854ead
tim <tjhickey@brandeis.edu>
2018-05-14 07:37:48 -0400

    added msetdemo without networking to the msetdemo app

--- msetdemo/app.js
+++ msetdemo/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var msetRouter = require('./routes/mset');
 
 //var app = require('express')();
 var app = express();
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mset', msetRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
